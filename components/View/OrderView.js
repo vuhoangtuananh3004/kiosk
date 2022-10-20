445009ff51b103ad54af512dc3202eb00e16fb3d
IAmARodent <100889347+IAmARodent@users.noreply.github.com>
2022-10-19 22:01:45 -0700

    Update OrderView.js

--- components/View/OrderView.js
+++ components/View/OrderView.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import ItemBox from '../CustomerView/ItemBox'
 
 function OrderView() {
 
   return (
-    <div>OrderView</div>
+    <div>
+      <div className="flex justify-center p-5">
+        <button className="bg-indigo-400 self-auto text-6xl hover:bg-sky-500 duration-200 p-5 m-5">
+          Change Menu
+        </button>
+        <button className="bg-indigo-400 self-auto text-6xl hover:bg-sky-500 duration-200 p-5 m-5">
+          Place Order
+        </button>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+        <ItemBox />
+      </div>
+    </div>
   )
 }
 
