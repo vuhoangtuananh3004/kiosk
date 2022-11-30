8dc6398c22b5d30a7a0fb8842cd740917f749f8b
IAmARodent <100889347+IAmARodent@users.noreply.github.com>
2022-11-29 22:01:09 -0800

    added cart and it persists through reload(localstorage)

--- components/View/OrderView.js
+++ components/View/OrderView.js
@@ -1,4 +1,4 @@
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import React, { useEffect, useState } from 'react'
 import ItemBox from '../CustomerView/ItemBox'
@@ -6,6 +6,18 @@ import db from '../../firebaseConfig'
 
 function OrderView() {
   const [menu, setMenu] = useState([])
+  var carting;
+  if (localStorage.getItem("cart") == null) {
+    carting = []
+  }
+  else {
+    carting = JSON.parse(localStorage.getItem("cart"))
+  }
+  const [cart, setCart] = useState(carting)
+  const addToCart = (item) => {
+    console.log(item)
+    setCart([...cart, item])
+  }
   async function addItemsFromDatabase() {
     const querySnapshot = await getDocs(collection(db, "menu"));
     const menus = [];
@@ -17,24 +29,38 @@ function OrderView() {
   useEffect(() => {
     addItemsFromDatabase()
   }, [])
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart))
+  }, [cart])
   return (
     <div>
-      <div className="flex justify-center p-5">
-        <button className="bg-indigo-400 self-auto text-6xl hover:bg-sky-500 duration-200 p-5 m-5">
-          Place Order
-        </button>
-      </div>
-      <div className="flex flex-wrap justify-center">
-        {menu.map((item) => (
-          <div className="relative p-5 m-5 h-96 w-96 shadow-2xl bg-gray-100" key={item.id}>
-            <div className="text-2xl font-bold">{item.name}</div>
-            <img src="https://media.istockphoto.com/photos/hamburger-with-cheese-and-french-fries-picture-id1188412964?k=20&m=1188412964&s=612x612&w=0&h=Ow-uMeygg90_1sxoCz-vh60SQDssmjP06uGXcZ2MzPY="></img>
-            <div>Juicy cheeseburger with a side of pickles</div>
-            <div className="flex justify-center">
-              <button className="bg-blue-300 p-2 hover:bg-gray-300 duration-200">Add to order</button>
+      <div className="flex flex-row">
+        <div className="flex flex-wrap justify-center basis-2/3">
+          {menu.map((item) => (
+            <div className="relative p-5 m-5 h-96 w-96 shadow-2xl bg-gray-100" key={item.id}>
+              <div className="flex flex-wrap justify-center">
+                <div className="text-2xl font-bold">{item.name}</div>
+                <div className="text-2xl font-bold pl-8">$19.99</div>
+              </div>
+              <img src="https://media.istockphoto.com/photos/hamburger-with-cheese-and-french-fries-picture-id1188412964?k=20&m=1188412964&s=612x612&w=0&h=Ow-uMeygg90_1sxoCz-vh60SQDssmjP06uGXcZ2MzPY="></img>
+              <div>Juicy cheeseburger with a side of pickles</div>
+              <div className="flex justify-center">
+                <button className="bg-blue-300 p-2 hover:bg-gray-300 duration-200" onClick={() => addToCart(item)}>Add to order</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div className="basis-1/3 text-center p-5">
+          {cart.map((item) => (
+            <div>
+              <div className="text-2xl font-bold">{item.name}</div>
+            </div>
+          ))}
+          <button className="bg-indigo-400 self-auto text-6xl hover:bg-sky-500 duration-200 p-5 m-5" onClick={() => console.log(cart)}>
+            Place Order
+          </button>
+
+        </div>
       </div>
     </div>
   )
